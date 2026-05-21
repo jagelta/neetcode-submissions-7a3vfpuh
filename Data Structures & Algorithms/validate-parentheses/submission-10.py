e0a48cb2c2e0efb17c499c95b71e3a398fa58082
Aakhash Ganesh <aakhashgv@gmail.com>
2026-05-21 15:22:25 -0400

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        tracker = []
+        for char in s:
+            print(tracker)
+            if char == '(':
+                tracker.append(')')
+            elif char == '[':
+                tracker.append(']')
+            elif char == '{':
+                tracker.append('}')
+            elif len(tracker) == 0 or char != tracker[-1]:
+                return False
+            elif char == tracker[-1]:
+                tracker.pop()
+        print(len(tracker))
+        return True if not tracker else False
